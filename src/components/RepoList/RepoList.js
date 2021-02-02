9f4b8b8d91f65c6f84213e1ca4211026a7b277a3
trulli90 <trulli90@gmail.com>
2021-02-02 19:40:19 +0100

    changed background and color of RepoList

--- src/components/RepoList/RepoList.js
+++ src/components/RepoList/RepoList.js
@@ -7,7 +7,7 @@ export default function RepoPage(props) {
         <ListGroup style={{position: "absolute", zIndex: "10", width: "100%", paddingRight: "30px", marginTop: "-1rem"}}>
             {repos
                 .filter((item, idx) => idx<5)
-                .map(repo => <ListGroup.Item key={repo}><a href={`https://github.com/anjakhan/${repo}`} rel="noreferrer" target="_blank" >{repo}</a></ListGroup.Item>)
+                .map(repo => <ListGroup.Item key={repo} variant="secondary"><a href={`https://github.com/anjakhan/${repo}`} rel="noreferrer" target="_blank" style={{color: "#980C13"}} >{repo}</a></ListGroup.Item>)
             }
       </ListGroup>
     );
